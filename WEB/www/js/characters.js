c31c5f75e7c635ed927cd0c53945c99cabe922e1
José Ricardo <zericardobraga@gmail.com>
2021-06-16 19:58:31 -0300

    listando os personagens cadastrados no JSON

--- WEB/www/js/characters.js
+++ WEB/www/js/characters.js
@@ -1,5 +1,6 @@
 import {
-    renderHtml
+    renderHtml,
+    criarElemento
 } from './util.js'
 
 export {
@@ -7,10 +8,70 @@ export {
 }
 
 async function renderCharacters() {
-    await renderHtml('list', 'main');
-    await fetch('http://localhost:3001/list')
-    // .then(response => response.json())
-    // .then(json => {
-    //     json.
-    // });
+    await renderHtml('character/list', 'main');
+    await fetch('http://localhost:3000/characters')
+    .then(response => response.json())
+    .then(json => {
+      json.forEach( character => {
+        const card = criarCard(character);
+        const section = document.querySelector('section.personagens-cadastrados');
+        section.appendChild(card);
+      });  
+    });
 };
+
+function criarCard(character){
+    const divCharacter = criarElemento('div')
+    .adicionarClasse('card'); 
+    
+    const spanId = criarElemento('span')
+    .adicionarClasse('card-titulo')
+    .adicionarTexto(character.id); 
+    
+    const img  = criarElemento('img');
+    img.src = "./images/" + character.image;
+    
+    const divConteudo = criarElemento('div')
+    .adicionarClasse('card-conteudo');
+    
+    const spanNome = criarElemento('span')
+    .adicionarClasse('card-dados')
+    .adicionarTexto(character.name);
+    
+    const spanRaca = criarElemento('span')
+    .adicionarClasse('card-dados')
+    .adicionarTexto(character.race);
+    
+    const spanLevel = criarElemento('span')
+    .adicionarClasse('card-dados')
+    .adicionarTexto(character.level);
+
+    const divAlterar = criarElemento('div')
+    .adicionarClasse('card__alterar');
+    
+    const btnEditar = document.createElement('button');
+    btnEditar.classList.add('btn')
+    btnEditar.innerHTML = "Editar";
+    btnEditar.onclick = function (e){
+        alert("Programar a função do botão");
+    }
+    
+    const btnExcluir = document.createElement('button');
+    btnExcluir.classList.add('btn');
+    btnExcluir.innerHTML = "Excluir";
+    btnExcluir.onclick = function (e){
+        alert("Programar a função do botão");
+    }
+
+    divCharacter.incluirFilho(spanId)
+    .incluirFilho(img)
+    .incluirFilho(divConteudo);
+
+    divConteudo.incluirFilho(spanNome)
+    .incluirFilho(spanRaca)
+    .incluirFilho(spanLevel)
+    .incluirFilho(btnEditar)
+    .incluirFilho(btnExcluir);
+
+    return divCharacter;
+}
--- WEB/www/js/util.js
+++ WEB/www/js/util.js
@@ -1,5 +1,7 @@
 export {
-    renderHtml
+    renderHtml,
+    criarElemento,
+    bindFunctions
 }
 
 async function renderHtml(path, seletor){
@@ -11,4 +13,34 @@ async function renderHtml(path, seletor){
           tag.innerHTML = texto;
         }
       );
+}
+
+// Cria um objeto html.
+function criarElemento(tipo){
+  const elemento = document.createElement(tipo);
+  bindFunctions(elemento);
+  return elemento;
+}
+
+// Vincula funções ao elemento;.
+function bindFunctions(elemento){
+  elemento.incluirFilho = incluirFilho;
+  elemento.adicionarClasse = adicionarClasse;
+  elemento.adicionarTexto = adicionarTexto;
+  return elemento;
+}
+
+function incluirFilho(filho){
+  this.appendChild(filho)
+  return this;
+}
+
+function adicionarClasse(classe){
+  this.classList.add(classe)
+  return this;
+}
+
+function adicionarTexto(texto){
+  this.innerHTML = texto
+  return this;
 }
