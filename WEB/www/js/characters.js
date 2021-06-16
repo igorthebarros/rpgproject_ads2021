70b7ade716db5d772e23175873355de1ebde1fab
Igor Barros <1igorhbarros@gmail.com>
2021-06-16 19:37:17 -0300

    character list

--- WEB/www/js/characters.js
+++ WEB/www/js/characters.js
@@ -1,16 +1,51 @@
 import {
-    renderHtml
+    renderHtml,
+    createElementCustom
 } from './util.js'
 
 export {
     renderCharacters
-}
+};
 
 async function renderCharacters() {
-    await renderHtml('list', 'main');
-    await fetch('http://localhost:3001/list')
-    // .then(response => response.json())
-    // .then(json => {
-    //     json.
-    // });
+    await renderHtml('character/list', 'main');
+    await fetch('http://localhost:3000/characters')
+    .then(response => response.json())
+    .then(json => {
+        json.forEach( character => {
+            const card = createCharacterCard(character);
+            const section = document.querySelector('section.personagens-cadastrados');
+            section.appendChild(card);     
+        })
+    });
 };
+
+function createCharacterCard(character){
+    const divCharacter = createElementCustom('div')
+        .addClassCustom('card-character');
+    const spanId = createElementCustom('span')
+        .addClassCustom('card-id')
+        .addTextCustom(character.id);
+    const img = createElementCustom('img');
+    img.src = "./img/" + character.image;
+    const divContent = createElementCustom('div')
+        .addClassCustom('card-conteudo');
+    const spanName = createElementCustom('span')
+        .addClassCustom('card-name')
+        .addTextCustom(character.name);
+    const spanRace = createElementCustom('span')
+        .addClassCustom('card-race')
+        .addTextCustom(character.race);
+    const spanLevel = createElementCustom('span')
+        .addClassCustom('card-level')
+        .addTextCustom(character.level);
+    
+    divContent.appendChildCustom(spanName)
+    .appendChildCustom(spanRace)
+    .appendChildCustom(spanLevel);
+    divCharacter.appendChildCustom(spanId)
+        .appendChildCustom(img)
+        .appendChildCustom(divContent);
+
+    return divCharacter;
+}
--- WEB/www/js/util.js
+++ WEB/www/js/util.js
@@ -1,14 +1,48 @@
 export {
-    renderHtml
+    renderHtml,
+    createElementCustom,
+    addClassCustom,
+    addTextCustom,
+    appendChildCustom
 }
 
 async function renderHtml(path, seletor){
-    const url = 'html/' + path + '.html';
-    await fetch(url)
-      .then(res => res.text())
-      .then(texto => {
-          const tag = document.querySelector(seletor);
-          tag.innerHTML = texto;
-        }
-      );
+  const url = 'html/' + path + '.html';
+  await fetch(url)
+    .then(res => res.text())
+    .then(texto => {
+        const tag = document.querySelector(seletor);
+        tag.innerHTML = texto;
+      }
+    );
 }
+
+function createElementCustom(type){
+    const element = document.createElement(type);
+    bindFunctions(element);
+    return element;
+}
+
+function bindFunctions(element){
+    element.appendChildCustom = appendChildCustom;
+    element.addClassCustom = addClassCustom;
+    element.addTextCustom = addTextCustom;
+    return element;
+  }
+
+function appendChildCustom(child){
+      this.appendChild(child)
+      return this;
+}
+
+function addClassCustom(classToAdd){
+    this.classList.add(classToAdd)
+    return this;
+}
+
+function addTextCustom(textToAdd){
+    this.innerHTML = textToAdd
+    return this;
+}
+
+  
